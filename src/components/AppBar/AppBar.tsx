e785a6785cd439e607bcdf6bae3bc5b05f465936
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-03-20 23:12:52 +0200

    add link to main page

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,11 +1,12 @@
 import { TbBrandJavascript } from 'react-icons/tb';
-import { AppBar, Toolbar, Box } from '@mui/material';
+import { AppBar, Toolbar } from '@mui/material';
 
 import { useAppSelector } from '../../hooks/hooks';
 import { selectServise } from '../../redux/selectors';
 
 import { SearchQuestion } from '../SearchQuestion/SearchQuestion';
 import { CategorySelector } from '../CategorySelector/CategorySelector';
+import { Link } from 'react-router-dom';
 
 export const MainAppBar = () => {
   const showSearch = useAppSelector(selectServise);
@@ -14,9 +15,9 @@ export const MainAppBar = () => {
     <>
       <AppBar position={showSearch === 'search' ? 'fixed' : 'static'}>
         <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Box sx={{ display: { xs: 'block', sm: 'none' } }}>
-            <TbBrandJavascript size={40} />
-          </Box>
+          <Link to="/">
+            <TbBrandJavascript size={40} color="white" />
+          </Link>
           {showSearch === 'search' && <SearchQuestion />}
           {showSearch === 'select' && <CategorySelector />}
         </Toolbar>
