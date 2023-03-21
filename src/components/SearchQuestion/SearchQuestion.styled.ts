e3345d846b7f1012073bc796bb00d1d350f796ab
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-03-21 16:55:45 +0200

    remove focus color from input

--- src/components/SearchQuestion/SearchQuestion.styled.ts
+++ src/components/SearchQuestion/SearchQuestion.styled.ts
@@ -1,17 +1,14 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
-export const EInput = styled.input`
-	display: block;
-	width: 100%;
-	height: 40px;
-	padding: 0 20px;
+export const SearchInput = styled.input`
+  display: block;
+  width: 100%;
+  height: 40px;
+  padding: 0 20px;
 
-	border: 2px solid red;
-	border-radius: 10px;
+  border-radius: 10px;
 
-	:focus {
-		border: 2px solid green;
-
-		outline: none;
-	}
+  :focus {
+    outline: none;
+  }
 `;
--- src/components/SearchQuestion/SearchQuestion.tsx
+++ src/components/SearchQuestion/SearchQuestion.tsx
@@ -3,13 +3,14 @@ import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { changeFilter } from '../../redux/filterSlice';
 import { selectFilter } from '../../redux/selectors';
 
-import { EInput } from './SearchQuestion.styled';
+import { SearchInput } from './SearchQuestion.styled';
 
 interface ISearchQuestionProps {
   color?: string;
 }
 
 export const SearchQuestion: FC<ISearchQuestionProps> = ({ color }) => {
+  console.log(color);
   const inputValue = useAppSelector(selectFilter);
   const dispatch = useAppDispatch();
 
@@ -21,7 +22,7 @@ export const SearchQuestion: FC<ISearchQuestionProps> = ({ color }) => {
 
   return (
     <>
-      <EInput
+      <SearchInput
         type="text"
         id="filter"
         value={inputValue}
