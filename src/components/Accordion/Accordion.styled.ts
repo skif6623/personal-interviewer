61d36e35b5b95e79924f0d90b72c9f7ca5b581a2
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-03-22 21:39:04 +0200

    code review styles

--- src/components/Accordion/Accordion.styled.ts
+++ src/components/Accordion/Accordion.styled.ts
@@ -1,12 +1,14 @@
-import { Accordion } from '@mui/material';
 import styled from '@emotion/styled';
+import { Accordion } from '@mui/material';
 
 export const AccordionWrapper = styled(Accordion)`
-  min-height: 65px;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  min-height: 65px;
+
   transition: transform 250ms ease;
+
   :hover {
     transform: scale(1.02);
   }
--- src/components/CategoryItem/CategoryItem.styled.ts
+++ src/components/CategoryItem/CategoryItem.styled.ts
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
 
-interface EProps {
+interface CatItem {
   image?: string;
   open?: boolean;
   color?: string;
@@ -10,53 +10,62 @@ interface EProps {
 
 export const ECategoryItem = styled.li`
   position: relative;
+
   display: flex;
-  height: ${({ open }: EProps) => (open ? 'calc(100vh - 416px)' : '90px')};
+  height: ${({ open }: CatItem) => (open ? 'calc(100vh - 416px)' : '90px')};
   padding: 15px;
-  background-image: url(${({ image }: EProps) => (image ? image : 'none')});
+
+  background-image: url(${({ image }: CatItem) => (image ? image : 'none')});
   background-repeat: no-repeat;
   background-size: cover;
+
   transition: height 500ms ease;
+
   overflow: hidden;
 `;
 
 export const ETitleWrap = styled.div`
-  opacity: ${({ open }: EProps) => (open ? '0' : '1')};
-  pointer-events: ${({ open }) => (open ? 'none' : 'auto')};
   height: 100%;
+
+  pointer-events: ${({ open }) => (open ? 'none' : 'auto')};
+  opacity: ${({ open }: CatItem) => (open ? '0' : '1')};
+
   transition: opacity 500ms ease;
 `;
 
 export const ETitleOverlay = styled.div`
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
+  height: 30px;
   padding: 10px 15px;
+
   background-color: white;
   border-radius: 20px;
-  height: 30px;
 `;
 
 export const ECategoryTitle = styled.h3`
   text-transform: uppercase;
-  color: ${({ color }: EProps) => (color ? color : 'black')};
+  color: ${({ color }: CatItem) => (color ? color : 'black')};
 `;
 
 export const ECenteredWrap = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%);
 
   display: flex;
   flex-direction: column;
   justify-content: center;
   gap: 30px;
+
+  transform: translate(-50%, -50%);
 `;
 
 export const EIconWrap = styled.div`
   position: relative;
-  top: ${({ open }: EProps) => (open ? '0' : '-100px')};
+  top: ${({ open }: CatItem) => (open ? '0' : '-100px')};
+
   display: flex;
   justify-content: center;
   align-items: center;
@@ -64,9 +73,9 @@ export const EIconWrap = styled.div`
   height: 170px;
 
   color: ${({ color }) => (color ? color : '#000000')};
-  background: #ffffff;
+  background-color: #ffffff;
   border-radius: 50%;
-  opacity: ${({ open }: EProps) => (open ? '1' : '0')};
+  opacity: ${({ open }: CatItem) => (open ? '1' : '0')};
   pointer-events: ${({ open }) => (open ? 'auto' : 'none')};
 
   transition: all 500ms ease 250ms;
@@ -74,26 +83,28 @@ export const EIconWrap = styled.div`
 
 export const ECategoryBtn = styled(Link)`
   position: relative;
-  bottom: ${({ open }: EProps) => (open ? '0' : '-100px')};
+  bottom: ${({ open }: CatItem) => (open ? '0' : '-100px')};
+
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 20px 43px;
 
+  font-style: normal;
+  font-size: 16px;
+  font-weight: 500;
   text-decoration: none;
   text-transform: uppercase;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 16px;
   line-height: 1.3;
-  color: #ffffff;
 
-  background: rgba(233, 233, 233, 0.4);
+  color: #ffffff;
+  background-color: rgba(233, 233, 233, 0.4);
   backdrop-filter: blur(10px);
   border: ${({ color }) => (color ? `2px solid ${color}` : '2px solid black')};
   border-radius: 4px;
-  opacity: ${({ open }: EProps) => (open ? '1' : '0')};
+  opacity: ${({ open }: CatItem) => (open ? '1' : '0')};
   pointer-events: ${({ open }) => (open ? 'auto' : 'none')};
+
   transition: bottom 500ms ease 250ms, opacity 500ms ease 250ms,
     box-shadow 250ms ease;
 
