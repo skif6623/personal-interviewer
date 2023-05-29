9d4fab01b9b5a7ec46910d209df939c6ad88dfee
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-05-29 19:53:41 +0300

    change title and favicon

--- src/components/Logo/Logo.styled.ts
+++ src/components/Logo/Logo.styled.ts
@@ -4,4 +4,13 @@ import { Link } from 'react-router-dom';
 export const LogoLink = styled(Link)`
   height: 40px;
   margin-right: 16px;
+
+  color: #f46a3b;
+
+  transition: color 250ms ease;
+
+  :hover,
+  :focus {
+    color: #da5d33;
+  }
 `;
