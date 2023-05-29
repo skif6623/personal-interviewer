fb973990db37eace4161da77f52f6e4ad2ead1fe
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-05-29 23:27:12 +0300

    revert

--- src/components/Logo/Logo.styled.js
+++ src/components/Logo/Logo.styled.js
@@ -4,13 +4,4 @@ import { Link } from 'react-router-dom';
 export const LogoLink = styled(Link)`
   height: 40px;
   margin-right: 16px;
-
-  color: #f46a3b;
-
-  transition: color 250ms ease;
-
-  :hover,
-  :focus {
-    color: #da5d33;
-  }
 `;
